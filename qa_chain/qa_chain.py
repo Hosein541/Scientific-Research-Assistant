71e50f0229c51d59175a0dffa270ea0b4d1de6ce
Hosein541 <huseinmohammadi83@gmail.com>
2026-06-26 11:24:52 +0330

    edit qa_chain.py

--- qa_chain/qa_chain.py
+++ qa_chain/qa_chain.py
@@ -402,23 +402,5 @@ def main(selected):
         }
     })
 
-    
-    # prompt = ChatPromptTemplate.from_messages([
-    #     ("system", """You are an assistant for question-answering tasks.
-    # Use the following pieces of retrieved context to answer the question.
-    # If you don't know the answer, just say that you don't know.
-    # answer {language} in and keep it concise."""),
-    #     ("human", """Question: {question}
-
-    # Context: {context}
-
-    # Answer:""")
-    # ])
-
-    # qa_chain = (
-    #     {"context": retriever,"language": RunnablePassthrough(), "question":RunnablePassthrough()}
-    #     | prompt
-    #     | llm
-    # )
 
     return retriever
